b9a19e8b877ddfd4d121c781fc6c983d7c61503c
stop2stare <emitremmus0235@163.com>
2016-07-07 16:17:52 +0800

    使用browserHistory

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,8 @@
 var React = require('react'),
 	Router = require('react-router').Router,
 	Route = require('react-router').Route,
-	hashHistory = require('react-router').hashHistory,
-	TopicList = require('./topicList/TopicList'),
+	browserHistory = require('react-router').browserHistory,
+	TopicList = require('./index/TopicList'),
 	TopicDetail = require('./topicDetail/TopicDetail');
 
 require('../public/less/index');
@@ -10,8 +10,8 @@ require('../public/less/index');
 var Cnode = React.createClass({
 	render: function() {
 		return (
-			<Router history={hashHistory}>
-				<Route path="/" component={TopicList} />
+			<Router history={browserHistory}>
+				<Route path="/(:tab)" component={TopicList} />
 				<Route path="/topic/:id" component={TopicDetail} />
 			</Router>
 		);
