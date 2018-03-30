c8455d40b2737e9aad73330af7a80468acb5e21c
stop2stare <emitremmus0235@163.com>
2018-03-30 12:08:03 +0800

    feat: add ScrollToTop components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
 import IndexPage from './IndexPage'
 import TopicDetail from './TopicDetail'
 import SignIn from './SignIn'
@@ -17,16 +18,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <NavBar />
-          <Route path="/" exact component={IndexPage} />
-          <Route path="/tab/:id" component={IndexPage} />
-          <Route path="/topic/:id" component={TopicDetail} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/user/:name" component={User} />
-          <ToTop />
-          <Footer />
-        </div>
+        <ScrollToTop>
+          <div>
+            <NavBar />
+            <Route path="/" exact component={IndexPage} />
+            <Route path="/tab/:id" component={IndexPage} />
+            <Route path="/topic/:id" component={TopicDetail} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/user/:name" component={User} />
+            <ToTop />
+            <Footer />
+          </div>
+        </ScrollToTop>
       </Router>
     )
   }
