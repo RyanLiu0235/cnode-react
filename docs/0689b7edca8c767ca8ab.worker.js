e98f9eb1f22e674bac7d90ca2d8bda55c3d1420e
stop2stare <emitremmus0235@163.com>
2018-04-02 23:25:57 +0800

    docs: build docs

--- docs/0689b7edca8c767ca8ab.worker.js
+++ docs/0689b7edca8c767ca8ab.worker.js
@@ -70,16 +70,33 @@
 const hljs = __webpack_require__(1)
 const unescape = __webpack_require__(179)
 
-onmessage = e => {
-  const { content } = e.data
-  const codeRE = /<code>([\s\S]*?)<\/code>/gm
-  const highlighted = content.replace(codeRE, (...args) => {
+const codeRE = /<code>([\s\S]*?)<\/code>/gm
+const handleHighlight = raw => {
+  return raw.replace(codeRE, (...args) => {
     const raw = args[1]
     const unescaped = unescape(raw)
     const { value } = hljs.highlight('js', unescaped)
     return `<code>${value}</code>`
   })
-  postMessage(highlighted)
+}
+
+const linksRE = /(<a href="\/user)/g
+const handleAddHash = raw => raw.replace(linksRE, (...args) => `<a href="#/user`)
+
+const handler = raw => handleAddHash(handleHighlight(raw))
+
+onmessage = e => {
+  const { content, replies } = e.data
+  const _content = handler(content)
+  const _replies = replies.map(reply => {
+    reply.content = handler(reply.content)
+    return reply
+  })
+
+  postMessage(Object.assign({}, e.data, {
+    content: _content,
+    replies: _replies
+  }))
 }
 
 
